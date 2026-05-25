ca5ce62cb618187441537556401fb617445531bc
jonanv <jonan-vargas23@hotmail.com>
2026-05-25 13:06:33 -0500

    ✨ Add JS and HTML of Singleton Pattern

---
+++ 1_singleton/js/singleton.js
@@ -0,0 +1,35 @@
+class Singleton {
+
+    constructor() {
+
+        this.random = Math.random();
+
+        console.log('Entrando al contructor');
+        if (Singleton.instance) {
+            console.log('Ya existe');
+            return Singleton.instance;
+        }
+
+        console.log('No existe y se crea');
+        Singleton.instance = this;
+    }
+
+    getInstance() {
+        return Singleton.instance;
+    }
+
+}
+
+const singleton1 = new Singleton();
+console.log(singleton1.random);
+
+const singleton2 = new Singleton();
+console.log(singleton2.random);
+
+const singleton3 = new Singleton();
+console.log(singleton3.random);
+
+console.log(singleton1 === singleton2);
+console.log(singleton2 === singleton3);
+
+console.log(singleton1.getInstance() === singleton2.getInstance());
