d39457468330704182a00e7b8ce5c42cc045e359
jonanv <jonan-vargas23@hotmail.com>
2026-06-01 19:02:44 -0500

    ✨ Add practice case of State Pattern in JavaScript

--- 06_state/js/ball_state.js
+++ 06_state/js/ball_state.js
@@ -1,3 +1,94 @@
 class Ball {
-    
+
+    constructor(context, canvas, ballSize) {
+        this.context = context;
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.ballSize = ballSize;
+        this.positionX = 0;
+        this.positionY = 0;
+
+        this.state = new State1();
+    }
+
+    setState(state) {
+        this.state = state;
+    }
+
+    print() {
+        this.state.print(this);
+    }
 }
+
+class State1 {
+    print(ball) {
+        ball.context.clearRect(0, 0, ball.width, ball.height);
+        ball.context.fillRect(
+            ball.positionX, 
+            ball.positionY, 
+            ball.ballSize, 
+            ball.ballSize
+        );
+    
+        if (ball.positionX < (ball.width - ball.ballSize)) 
+            ball.positionX += ball.ballSize;
+        else
+            ball.setState(new State2());
+    }
+}
+
+class State2 {
+    print(ball) {
+        ball.context.clearRect(0, 0, ball.width, ball.height);
+        ball.context.fillRect(
+            ball.positionX, 
+            ball.positionY, 
+            ball.ballSize, 
+            ball.ballSize
+        );
+    
+        if (ball.positionY < (ball.height - ball.ballSize)) 
+            ball.positionY += ball.ballSize;
+        else
+            ball.setState(new State3());
+    }
+}
+
+class State3 {
+    print(ball) {
+        ball.context.clearRect(0, 0, ball.width, ball.height);
+        ball.context.fillRect(
+            ball.positionX, 
+            ball.positionY, 
+            ball.ballSize, 
+            ball.ballSize
+        );
+    
+        if (ball.positionX > 0) 
+            ball.positionX -= ball.ballSize;
+        else
+            ball.setState(new State4());
+    }
+}
+
+class State4 {
+    print(ball) {
+        ball.context.clearRect(0, 0, ball.width, ball.height);
+        ball.context.fillRect(
+            ball.positionX, 
+            ball.positionY, 
+            ball.ballSize, 
+            ball.ballSize
+        );
+    
+        if (ball.positionY > 0) 
+            ball.positionY -= ball.ballSize;
+        else
+            ball.setState(new State1());
+    }
+}
+
+const context = canvas.getContext('2d');
+context.fillStyle = 'black';
+const ball = new Ball(context, canvas, 10);
+setInterval(() => ball.print(), 100);
